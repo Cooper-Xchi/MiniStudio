1a0f2df2c769efcb149d116cb02aae56ef73f371
Cooper-Xchi <182845616+Cooper-Xchi@users.noreply.github.com>
2026-09-03 17:13:14 +0800

    lesson: verify OpenGL 4.1 core context

--- src/main.cpp
+++ src/main.cpp
@@ -1,19 +1,37 @@
 #include <iostream>
 #include <GLFW/glfw3.h>
 
-int run() {
+bool Init() {
     int result = glfwInit();
     if (result == GLFW_FALSE) {
         std::cerr << "GLFW initialization failed!" << std::endl;
-        return 1;
+        return false;
     }
     std::cout << "GLFW initialized!" << std::endl;
+    return true;
+}
+
+void SetContextHints() {
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 4);
+    glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 1);
+    glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+    glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GLFW_TRUE);
+}
+
+int Render() {
     GLFWwindow* window = glfwCreateWindow(800, 600, "MiniStudio", nullptr, nullptr);
     if (window == nullptr) {
         std::cerr << "GLFW Window creation failed!" << std::endl;
         glfwTerminate();
         return 1;
     }
+    glfwMakeContextCurrent(window);
+    int major = glfwGetWindowAttrib(window,GLFW_CONTEXT_VERSION_MAJOR);
+    int minor = glfwGetWindowAttrib(window,GLFW_CONTEXT_VERSION_MINOR);
+    int profile = glfwGetWindowAttrib(window, GLFW_OPENGL_PROFILE);
+    bool is_core_profile = profile == GLFW_OPENGL_CORE_PROFILE;
+    std::cout << "GLFW context version " << major << "." << minor << std::endl;
+    std::cout << "GLFW core profile equal profile? " << is_core_profile << std::endl;
     std::cout << "GLFW Window created!" << std::endl;
     while (!glfwWindowShouldClose(window)) {
         glfwPollEvents();
@@ -24,5 +42,7 @@ int run() {
 }
 
 int main() {
-    return run();
+    if (!Init()) return 1;
+    SetContextHints();
+    return Render();
 }
